bea20ffb88300509e4a7885cafd1c2aa6af82f29
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2026-01-13 12:01:37 -0300

    arreglo de paquetes

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,6 @@
-package com.tuempresa.tuapp.controller;
+package com.example.demo.controller;
 
-mport org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
